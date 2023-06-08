71ba76144da0fa3bf6e4448fd732e8664b47cdcc
Md Alamin <miraajehossain@gmail.com>
2023-06-08 14:56:15 +0600

    addtion,subtraction, multiplication done

--- src/components/calculator.component.tsx
+++ src/components/calculator.component.tsx
@@ -1,54 +1,82 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import {Button, ButtonGroup, Col, Row} from "react-bootstrap";
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 function Calculator() {
-    const [firstValue, setFirstValue ] = useState(0);
-    const [secondValue, setSecondValue ] = useState(0);
+    const [firstValue, setFirstValue] = useState(0);
+    const [secondValue, setSecondValue] = useState(0);
+    const [result, setResult] = useState(0);
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+    }
+
+    function handleAddition() {
+        setResult(firstValue + secondValue);
+    }
+
+    function handleSubtraction() {
+        setResult(firstValue - secondValue);
+    }
+
+    function handleMultiplication() {
+        setResult(firstValue * secondValue);
+    }
+
     return (
         <>
             <div>
                 <h1 className="text-center">Calculate whatever you want</h1>
             </div>
             <div>
-                <Row >
-                    <Col md={8} lg={8} sm={8}>
-                        <Row>
-                            <Col md={6} lg={6} sm={6}>
-                                <div>
+                <Form onSubmit={handleSubmit}>
+                    <Row>
 
+                        <Col md={8} lg={8} sm={8}>
+                            <Row>
+                                <Col md={6} lg={6} sm={6}>
+                                    <div>
+                                        <FloatingLabel
+                                            controlId="floatingInput"
+                                            label="Enter the value"
+                                            className="mb-3"
+                                        >
+                                            <Form.Control type="number" name="firstValue" value={firstValue}
+                                                          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                                              setFirstValue(+event.target.value)
+                                                          }}
+                                                          placeholder="0.00"/>
+                                        </FloatingLabel>
+                                    </div>
+                                </Col>
+                                <Col md={6} lg={6} sm={6}>
                                     <FloatingLabel
                                         controlId="floatingInput"
                                         label="Enter the value"
                                         className="mb-3"
                                     >
-                                        <Form.Control type="number" defaultValue={firstValue} placeholder="0.00" />
+                                        <Form.Control type="number" name="secondValue" value={secondValue}
+                                                      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                                          setSecondValue(+event.target.value)
+                                                      }}
+                                                      placeholder="0.00"/>
                                     </FloatingLabel>
-                                </div>
+                                </Col>
+                            </Row>
+                            <Col>
+                                <h1 className="text-center">Result {result}</h1>
                             </Col>
-                            <Col md={6} lg={6} sm={6}>
-                                <FloatingLabel
-                                    controlId="floatingInput"
-                                    label="Enter the value"
-                                    className="mb-3"
-                                >
-                                    <Form.Control type="number" defaultValue={secondValue} placeholder="0.00" />
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-                        <Col>
-                            <h1 className="text-center">Result......</h1>
                         </Col>
-                    </Col>
-                    <Col md={4} lg={4} sm={4}>
-                        <ButtonGroup size="lg"  aria-label="Basic example">
-                            <Button variant="secondary">+</Button>
-                            <Button variant="secondary">-</Button>
-                            <Button variant="secondary">X</Button>
-                        </ButtonGroup>
-                    </Col>
-                </Row>
+                        <Col md={4} lg={4} sm={4}>
+                            <ButtonGroup size="lg" aria-label="Basic example">
+                                <Button variant="secondary" name="addition" onClick={handleAddition}>+</Button>
+                                <Button variant="secondary" onClick={handleSubtraction}>-</Button>
+                                <Button variant="secondary" onClick={handleMultiplication}>X</Button>
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
+                </Form>
             </div>
         </>
     );
