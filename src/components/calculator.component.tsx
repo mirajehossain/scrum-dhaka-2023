2885062b26528d6ded310ca9adf56f82a455a456
hiroksarker <hirok.sarker@gmail.com>
2023-06-09 07:51:37 +0600

    add test cases

--- src/components/calculator.component.tsx
+++ src/components/calculator.component.tsx
@@ -1,16 +1,10 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import {Button, ButtonGroup, Col, Row} from "react-bootstrap";
-import React, {ChangeEvent, useState} from "react";
+import React, { ChangeEvent, useState } from 'react';
+import { Button, ButtonGroup, Col, Container, Form, Row } from 'react-bootstrap';
 
 function Calculator() {
     const [firstValue, setFirstValue] = useState(0);
     const [secondValue, setSecondValue] = useState(0);
     const [result, setResult] = useState(0);
-
-    function handleSubmit(event: any) {
-        event.preventDefault();
-    }
 
     function handleAddition() {
         setResult(firstValue + secondValue);
@@ -25,60 +19,63 @@ function Calculator() {
     }
 
     return (
-        <>
+        <Container>
             <div>
                 <h1 className="text-center">Calculate whatever you want</h1>
             </div>
-            <div>
-                <Form onSubmit={handleSubmit}>
-                    <Row>
+            <Form>
+                <Row>
+                    <Col md={6}>
+                        <Form.Group controlId="floatingInputFirst">
+                            <Form.Label>Enter the first value</Form.Label>
+                            <Form.Control
+                                type="number"
+                                name="firstValue"
+                                value={firstValue}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                    setFirstValue(+event.target.value);
+                                }}
+                                placeholder="0.00"
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group controlId="floatingInputSecond">
+                            <Form.Label>Enter the second value</Form.Label>
+                            <Form.Control
+                                type="number"
+                                name="secondValue"
+                                value={secondValue}
+                                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                    setSecondValue(+event.target.value);
+                                }}
+                                placeholder="0.00"
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Form>
 
-                        <Col md={8} lg={8} sm={8}>
-                            <Row>
-                                <Col md={6} lg={6} sm={6}>
-                                    <div>
-                                        <FloatingLabel
-                                            controlId="floatingInput"
-                                            label="Enter the value"
-                                            className="mb-3"
-                                        >
-                                            <Form.Control type="number" name="firstValue" value={firstValue}
-                                                          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                                                              setFirstValue(+event.target.value)
-                                                          }}
-                                                          placeholder="0.00"/>
-                                        </FloatingLabel>
-                                    </div>
-                                </Col>
-                                <Col md={6} lg={6} sm={6}>
-                                    <FloatingLabel
-                                        controlId="floatingInput"
-                                        label="Enter the value"
-                                        className="mb-3"
-                                    >
-                                        <Form.Control type="number" name="secondValue" value={secondValue}
-                                                      onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                                                          setSecondValue(+event.target.value)
-                                                      }}
-                                                      placeholder="0.00"/>
-                                    </FloatingLabel>
-                                </Col>
-                            </Row>
-                            <Col>
-                                <h1 className="text-center">Result {result}</h1>
-                            </Col>
-                        </Col>
-                        <Col md={4} lg={4} sm={4}>
-                            <ButtonGroup size="lg" aria-label="Basic example">
-                                <Button variant="secondary" name="addition" onClick={handleAddition}>+</Button>
-                                <Button variant="secondary" onClick={handleSubtraction}>-</Button>
-                                <Button variant="secondary" onClick={handleMultiplication}>X</Button>
-                            </ButtonGroup>
-                        </Col>
-                    </Row>
-                </Form>
-            </div>
-        </>
+            <Row className="justify-content-center">
+                <ButtonGroup size="lg">
+                    <Button variant="secondary" onClick={handleAddition}>
+                        +
+                    </Button>
+                    <Button variant="secondary" onClick={handleSubtraction}>
+                        -
+                    </Button>
+                    <Button variant="secondary" onClick={handleMultiplication}>
+                        X
+                    </Button>
+                </ButtonGroup>
+            </Row>
+
+            <Row>
+                <Col md={12}>
+                    <h1 className="text-center">Result {result}</h1>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
